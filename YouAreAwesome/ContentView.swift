6fecc11d3b4339794ea89b8267302540d58a1231
PaulChen <csz890914@gmail.com>
2023-07-12 14:54:30 +0800

    course ch1.9 and challange

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,45 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var messageString = "Namaste"
+//    @State private var pressCounter = 0 //2023/7/12練習 我的寫法
     
     var body: some View {
         GeometryReader {geometry in //空間大小閱讀器 利用一個變數去抓螢幕大小
             ZStack { //前後重疊
-//                Rectangle()//方形色塊填色
-//                    .fill(
-//                        //Color("SkyBlue").opacity(0.4)天藍色 不透明度0.4
-//                        Gradient(colors: [.white,Color("SkyBlue")])//顏色漸變 數量不限
-//                    )
-//                    .ignoresSafeArea()//忽略安全區 滿版
+                //                Rectangle()//方形色塊填色
+                //                    .fill(
+                //                        //Color("SkyBlue").opacity(0.4)天藍色 不透明度0.4
+                //                        Gradient(colors: [.white,Color("SkyBlue")])//顏色漸變 數量不限
+                //                    )
+                //                    .ignoresSafeArea()//忽略安全區 滿版
                 VStack{ //VStack(spacing: 0) 上下空格為零
                     //Stack只能放10個 用Group包起來就只算一個 可以重複包 更改屬性一次針對全部
-//                    Group {
-//                        Text("I am a Text View!")
-//                        Text("I am a Text View!")
-//                        Text("I am a Text View!")
-//                        Text("I am a Text View!")
-//                        Text("I am a Text View!")
-//                        Text("I am a Text View!")
-//                        Text("I am a Text View!")
-//                        Text("I am a Text View!")
-//                        Text("I am a Text View!")
-//                        Group{
-//                            Text("I am a Text View!")
-//                            Text("I am a Text View!")
-//                            Text("I am a Text View!")
-//                            Text("I am a Text View!")
-//                        }
-//                    }
-//                    .font(.title)
-//                    .fontWeight(.heavy)
-
-//                    Text("You Have Skills!")
-//                        .font(.largeTitle)
-//                        .fontWeight(.black)//最粗
-//                        .foregroundColor(Color("SweetOrange"))//到Assets中字定義顏色
-//                        .padding()//圓角與安全區設置-1
-//                        .background(Color("Lime"))
-//                        .cornerRadius(15)//圓角與安全區設置-2
+                    //                    Group {
+                    //                        Text("I am a Text View!")
+                    //                        Text("I am a Text View!")
+                    //                        Text("I am a Text View!")
+                    //                        Text("I am a Text View!")
+                    //                        Text("I am a Text View!")
+                    //                        Text("I am a Text View!")
+                    //                        Text("I am a Text View!")
+                    //                        Text("I am a Text View!")
+                    //                        Text("I am a Text View!")
+                    //                        Group{
+                    //                            Text("I am a Text View!")
+                    //                            Text("I am a Text View!")
+                    //                            Text("I am a Text View!")
+                    //                            Text("I am a Text View!")
+                    //                        }
+                    //                    }
+                    //                    .font(.title)
+                    //                    .fontWeight(.heavy)
+                    
+                    //                    Text("You Have Skills!")
+                    //                        .font(.largeTitle)
+                    //                        .fontWeight(.black)//最粗
+                    //                        .foregroundColor(Color("SweetOrange"))//到Assets中字定義顏色
+                    //                        .padding()//圓角與安全區設置-1
+                    //                        .background(Color("Lime"))
+                    //                        .cornerRadius(15)//圓角與安全區設置-2
                     
                     Spacer()//空間等分空格填充
                     
@@ -56,10 +57,10 @@ struct ContentView: View {
                         .resizable()
                         .scaledToFit()
                     //黑灰白三色階
-//                        .symbolRenderingMode(.hierarchical)
+                    //                        .symbolRenderingMode(.hierarchical)
                     //自定義三色
-//                        .symbolRenderingMode(.palette)
-//                        .foregroundStyle(.gray, .orange, .blue)
+                    //                        .symbolRenderingMode(.palette)
+                    //                        .foregroundStyle(.gray, .orange, .blue)
                     //三色
                         .symbolRenderingMode(.multicolor)
                         .padding()
@@ -75,7 +76,7 @@ struct ContentView: View {
                                 .stroke(.teal, lineWidth:2)//描邊
                         )
                     //剪成圓形
-                        //.clipShape(Circle())
+                    //.clipShape(Circle())
                         .padding()
                     
                     Text(messageString)
@@ -84,7 +85,7 @@ struct ContentView: View {
                         .minimumScaleFactor(0.5)//過大自動縮小 直至符合
                         .multilineTextAlignment(.center)//多行自動對齊
                         .foregroundColor(Color.pink)
-                        //.italic()//斜體
+                    //.italic()//斜體
                         .frame(height: 150) //固定文字框大小
                         .frame(maxWidth:.infinity)//自動調整到最寬
                     //                .border(.brown, width: 1)//檢查用框框
@@ -92,28 +93,50 @@ struct ContentView: View {
                     
                     Spacer()//空間等分空格填充
                     
-//                    Divider()//分隔線用法1
-//                        .background(.black)
-//                        .padding()
-//                        .frame(width: 150.0) //寬度固定
-//
-//                    Rectangle()//分隔線用法2
-//                        .fill(Color("SweetOrange"))
-//                        .frame(width: geometry.size.width*(2/3), height: 3)//寬度自訂
+                    //                    Divider()//分隔線用法1
+                    //                        .background(.black)
+                    //                        .padding()
+                    //                        .frame(width: 150.0) //寬度固定
+                    //
+                    //                    Rectangle()//分隔線用法2
+                    //                        .fill(Color("SweetOrange"))
+                    //                        .frame(width: geometry.size.width*(2/3), height: 3)//寬度自訂
                     HStack{
-                        Button("Awesome") {
+                        Button("Show Message") {
                             //action when button is pressed
-                            messageString = "You Are Awesome!"
+                            //利用let
+                            let message1 = "You Are Awesome!"
+                            let message2 = "You Are Great!"
+                            
+                            if messageString == message1
+                            {
+                                messageString = message2
+                            }
+                            else
+                            {
+                                messageString = message1
+                            }
+                            //2023/7/12練習 我的寫法
+//                            if pressCounter == 0
+//                            {
+//                                messageString = "You Are Awesome!"
+//                                pressCounter = 1
+//                            }
+//                            else
+//                            {
+//                                messageString = "You Are Great!"
+//                                pressCounter = 0
+//                            }
                         }
                         .buttonStyle(.borderedProminent) //設定按鈕格式
                         
-                        Spacer()
-                        
-                        Button("Great") {
-                            //action when button is pressed
-                            messageString = "You Are Great!"
-                        }
-                        .buttonStyle(.borderedProminent)
+                        //                        Spacer()
+                        //
+                        //                        Button("Great") {
+                        //                            //action when button is pressed
+                        //                            messageString = "You Are Great!"
+                        //                        }
+                        //                        .buttonStyle(.borderedProminent)
                     }
                     //            .border(.brown, width: 5)//檢查用框框
                     .padding()
